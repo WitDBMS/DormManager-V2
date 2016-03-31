abeb2f159ef2ee21c6593d447140468f8024d0d8
Jake Mathews <mathewsj2@wit.edu>
2016-03-31 08:36:33 -0400

    Modified package.json Successful login with MySQL

--- dormmanager/dormmanager/routes/index.js
+++ dormmanager/dormmanager/routes/index.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+// MySQL Setup
+var mysql = require('mysql');
+var connection = mysql.createConnection(
+    {
+      host     : 'jakesserver',
+      user     : 'jake',
+      password : 'jake',
+      database : 'dorm',
+    }
+);
+connection.connect();
+ 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
@@ -8,12 +20,24 @@ router.get('/', function(req, res, next) {
 });
 
 router.post("/login", function(req, res, next) {
+  // A login was attepted
+  console.log("\nLogin Attempted");
   console.log(req.body);
-  if(req.body.password==="abc") {
-    res.redirect("test.html");
-  } else {
-    res.send({type: "joby is a numnut"});
-  }
+
+  // Generate login QUERY
+  var id = req.body.id;
+  var password = req.body.password;
+  var query = "SELECT * FROM students WHERE ID='" + id + "' AND Password='" + password + "'";
+  connection.query(query, function(err, rows, fields) {
+    console.log(query);
+    if(err) throw err;
+    if(!isNaN(id) && rows.length > 0) {
+      res.send({type: "success", data: "student/g/"});
+    } else {
+      res.send({type: "fail", data: "Invalid username or password"});
+    }
+  });
 });
+
 
 module.exports = router;
